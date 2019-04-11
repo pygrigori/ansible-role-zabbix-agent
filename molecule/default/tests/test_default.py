a4167fb661589cbf7ced4ac64d8a059371dda2d1
Grigori Py <pygrigori@gmail.com>
2019-04-11 17:55:37 +0300

    molecule tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,22 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_pkg_installed(host):
+    package = host.package('zabbix-agent')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert package.is_installed
+
+
+def test_service_is_enabled(host):
+    service = host.service('zabbix-agent')
+
+    assert service.is_running
+    assert service.is_enabled
+
+
+def test_config_exists(host):
+    file = host.file('/etc/zabbix/zabbix_agentd.conf')
+
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'root'
